efa06fb01099e8a103b3f0a5205faf2095c5ce59
sl693 <sl693@exeter.ac.uk>
2024-07-03 19:33:18 +0100

    Merge RRBS and Array results attempt

---
+++ 3_ArrayRRBSComparison/1_merge_finalise.R
@@ -0,0 +1,66 @@
+#!/usr/bin/env Rscript
+## ----------Script-----------------  
+##
+## Purpose: Process and merge RRBS and array data from rTg4510 and J20
+##          Common probes between the array and RRBS
+## 
+## Date:    03 July 2024        
+##
+## Author: Szi Kay Leung (S.K.Leung@exeter.ac.uk)
+##
+## ---------- Notes -----------------
+
+# directory input
+rootDir = "/lustre/projects/Research_Project-191406"
+scriptDir = "/lustre/projects/Research_Project-MRC148213/lsl693/scripts/rrbs-ad-mice/"
+source(paste0(scriptDir, "import.config"))
+source(paste0(scriptDir, "3_ArrayRRBSComparison/functions/summaryStatsDMP.R"))
+
+#-------------- input -------------
+
+## smoothed BiSeq RRBS dataset
+rTg4510_rrbs_beta <- get(load(file = paste0(dirnames$processed, "/rrbs/rTg4510_RRBS_SmoothBetas.RData")))
+rTg4510_array_beta <- get(load(file = paste0(dirnames$processed, "/array/Normalised_rTg4510_array_ECX.RData")))
+J20_rrbs_beta <- get(load(file = paste0(dirnames$processed, "/rrbs/J20_RRBS_SmoothBetas.RData")))
+J20_array_beta <- get(load(file = paste0(dirnames$processed, "/array/Normalised_J20_array_ECX.RData")))
+
+# remove probes with no chromosomal position (due to patch)
+# duplicated probes per position in array; take average
+rTg4510_array_beta <- rTg4510_array_beta[!is.na(rTg4510_array_beta$position),] %>% dplyr::select(-Gene_Symbol, -annotation, -cpg) 
+rTg4510_array_beta <- as.data.frame(rTg4510_array_beta %>% group_by(position) %>% summarise_all("mean")) %>% tibble::column_to_rownames(., var = "position")
+J20_array_beta <- J20_array_beta[!is.na(J20_array_beta$position),] %>% dplyr::select(-Gene_Symbol, -annotation, -cpg) 
+J20_array_beta <- as.data.frame(J20_array_beta %>% group_by(position) %>% summarise_all("mean")) %>% tibble::column_to_rownames(., var = "position")
+# save datawrangled results
+save(rTg4510_array_beta, file = paste0(dirnames$processed, "/array/Normalised_rTg4510_array_ECX_Final.RData"))
+save(J20_array_beta, file = paste0(dirnames$processed, "/array/Normalised_J20_array_ECX_Final.RData"))
+
+## differential all results
+rTg4510_rrbs_results <- get(load( file = paste0(dirnames$differential, "/rrbs/rTg4510_betaResultsDMPs.RData")))
+J20_rrbs_results <- get(load( file = paste0(dirnames$differential, "/rrbs/J20_betaResultsDMPs.RData")))
+
+## annotated significant results
+rTg4510_rrbs_sig <- get(load(file = paste0(dirnames$annotated, "/rrbs/rTg4510_rrbs_annoSigResultsDMPs.RData")))
+rTg4510_array_sig <- get(load(file = paste0(dirnames$annotated, "/array/rTg4510_array_annoSigResultsDMPs.RData")))
+
+J20_rrbs_sig <- get(load(file = paste0(dirnames$annotated, "/rrbs/J20_rrbs_annoSigResultsDMPs.RData")))
+J20_array_sig <- get(load(file = paste0(dirnames$annotated, "/array/J20_array_annoSigResultsDMPs.RData")))
+
+# merge RRBS and Array results
+rTg4510_merge_sig <- MergeArrayRRBSSigResults(rrbsBeta=rTg4510_rrbs_beta, 
+                                              arrayBeta=rTg4510_array_beta, 
+                                              rrbsSigResults=rTg4510_rrbs_sig, 
+                                              arraySigResults=rTg4510_array_sig, 
+                                              phenotypeInput=phenotype$rTg4510)
+J20_merge_sig <- MergeArrayRRBSSigResults(J20_rrbs_beta, J20_array_beta, J20_rrbs_sig, J20_array_sig, phenotype$J20)
+
+# save signfiicant results
+rTg4510_ECX_sigResultsFull <- rTg4510_merge_sig$sig_ECX_Full
+J20_ECX_sigResultsFull <- J20_merge_sig$sig_ECX_Full
+save(rTg4510_ECX_sigResultsFull, file = paste0(dirnames$annotated,"/final/rTg4510_ECX_sigResultsDMPs.RData"))
+save(J20_ECX_sigResultsFull, file = paste0(dirnames$annotated,"/final/J20_ECX_sigResultsDMPs.RData"))
+
+# save significant beta 
+rTg4510_sig_betaFull <- rTg4510_merge_sig$sig_betaECX_Full
+J20_sig_betaFull  <- J20_merge_sig$sig_betaECX_Full
+save(rTg4510_sig_betaFull, file = paste0(dirnames$annotated,"/final/rTg4510_ECX_sigBeta.RData"))
+save(J20_sig_betaFull, file = paste0(dirnames$annotated,"/final/J20_ECX_sigBeta.RData"))
---
+++ 3_ArrayRRBSComparison/functions/summaryStatsDMP.R
@@ -0,0 +1,231 @@
+LOGEN_ROOT="/lustre/projects/Research_Project-MRC148213/lsl693/scripts/LOGen"
+source(paste0(LOGEN_ROOT, "/aesthetics_basics_plots/draw_density.R"))
+source(paste0(LOGEN_ROOT, "/aesthetics_basics_plots/pthemes.R"))
+
+suppressMessages(library("stringr"))
+suppressMessages(library("dplyr"))
+
+# common probes between beta dataset and number of probes from array
+commonStatsDescription <- function(rrbsBeta, arrayBeta){
+  message("Number of probes in RRBS after smoothing: ", length(row.names(rrbsBeta)))
+  message("Number of probes in array detected (Horvath mouse array): ", length(row.names(arrayBeta)))
+  common_probes <- intersect(row.names(rrbsBeta), row.names(arrayBeta))
+  message("Number of common probes: ", length(common_probes))
+  message("% common probes in RRBS: ", round(length(common_probes)/length(row.names(rrbsBeta)) * 100, 2))
+  message("% common probes in RRBS: ", round(length(common_probes)/length(row.names(arrayBeta)) * 100, 2))
+  
+  return(common_probes)
+}
+
+# correlation plot of the common probes between RRBS and Array
+corrPlotCommonProbes <- function(rrbsBeta, arrayBeta, commonProbes){
+  common <- list(
+    rrbs = rrbsBeta[commonProbes,] %>% apply(., 1, mean, na.rm=TRUE) %>% reshape2::melt(value.name = "RRBS"),
+    array = arrayBeta[commonProbes,] %>% apply(., 1, mean, na.rm=TRUE) %>% reshape2::melt(value.name = "Array")
+  )
+  
+  pCommonCorr <- density_plot(merge(common$rrbs, common$array, by = 0),
+                              x.var = "RRBS", y.var = "Array", x_lab = "RRBS - mean methylation", y_lab = "Array - mean methylation", title = "") + 
+    scale_y_continuous(trans = "log10") + scale_x_continuous(trans = "log10")
+  
+  return(pCommonCorr)
+}
+
+
+# calculate absolute difference between TG and WT
+extract_delta <- function(betaMatrix, sigMatrix = FALSE, phenotypeFile, position = NULL){
+  if(!isFALSE(sigMatrix)){
+    dat <- betaMatrix %>% filter(row.names(betaMatrix) %in% sigMatrix[["position"]])
+  }else{
+    dat <- betaMatrix %>% filter(row.names(betaMatrix) %in% position)
+  }
+  
+  
+  if(!is.null(sigMatrix)){
+    colnames(dat) <- unlist(lapply(colnames(betaMatrix), function(x) paste0(x,"_", phenotypeFile[row.names(phenotypeFile) == x,"Genotype"])))
+    dat <- dat %>% tibble::rownames_to_column(., var = "position") %>% reshape2::melt(variable.name = "sample",value.name = "methylation", id = "position")
+    dat <- dat %>% dplyr::mutate(Genotype = word(as.character(sample), c(2), sep = stringr::fixed("_")))
+    dat <- dat %>% group_by(position, Genotype) %>% dplyr::summarise(mean = mean(methylation, na.rm = T), .groups = 'keep') 
+    dat <- tidyr::spread(dat, Genotype, mean) %>% mutate(delta = TG - WT, 
+                                                         directionTG = ifelse(delta < 0, "down", "up"))
+    dat <- dat %>% dplyr::rename(meanTG = TG, meanWT = WT)
+  }else{
+    dat <- data.frame()
+  }
+  
+  return(dat)
+}
+
+
+MergeArrayRRBSSigResults <- function(rrbsBeta, arrayBeta, rrbsSigResults, arraySigResults, phenotypeInput){
+  
+  # delta
+  # calculate absolute difference between TG and WT --> delta = TG - WT
+  # therefore negative delta, decrease methylation in TG
+  message("Calculating delta difference between WT and TG")
+  delta <- list(
+    Genotype = list(
+      array = extract_delta(betaMatrix = arrayBeta, sigMatrix = arraySigResults$ECX$Genotype, phenotypeFile = phenotypeInput),
+      rrbs = extract_delta(betaMatrix = rrbsBeta, sigMatrix = rrbsSigResults$Genotype, phenotypeFile = phenotypeInput)
+    ),
+    Interaction = list(
+      array = extract_delta(betaMatrix = arrayBeta, sigMatrix = arraySigResults$ECX$Interaction, phenotypeFile = phenotypeInput),
+      rrbs = extract_delta(betaMatrix = rrbsBeta, sigMatrix = rrbsSigResults$Interaction, phenotypeFile = phenotypeInput)
+    ),
+    Pathology = list(
+      array = extract_delta(betaMatrix = arrayBeta, sigMatrix = arraySigResults$ECX$Pathology, phenotypeFile = phenotypeInput),
+      rrbs = extract_delta(betaMatrix = rrbsBeta, sigMatrix = rrbsSigResults$Pathology, phenotypeFile = phenotypeInput)
+    )
+  )
+  
+  ## merge array and RRBS results for genotype: ECX
+  # arrange by P.value rather than FDR as merging array and RRBS results with different significant values
+  # i.e. array would be more sensitive with higher FDR than RRBS given fewer sites (reduced multiple testing)
+  cols2Keep <- c("meanWT", "meanTG", "delta", "directionTG", "ChIPseeker_GeneEnsembl","ChIPseeker_GeneSymbol", "ChIPseeker_GeneName","ChIPseeker_Annotation","ChIPseeker_TransEnsembl","distanceToTSS")
+  
+  # Process Genotype data
+  message("Merge ECX Genotype")
+  sig_ECX_Genotype <- rbind(
+    mergeSigResults("Genotype", arraySigResults$ECX$Genotype, "Array", delta$Genotype$array, "PrZ.GenotypeTG", "FDR_adj_Genotype", cols2Keep),
+    mergeSigResults("Genotype",rrbsSigResults$Genotype, "RRBS", delta$Genotype$rrbs, "p.val.Genotype", "FDR_adj_genotype", cols2Keep)
+  ) %>% arrange(P.value_Genotype) 
+  
+  # Process GenotypeAge data
+  message("Merge ECX GenotypeAge")
+  sig_ECX_GenotypeAge <- rbind(
+    mergeSigResults("GenotypeAge", arraySigResults$ECX$Interaction, "Array", delta$Interaction$array, "PrZ.GenotypeTG.Age_months", "FDR_adj_GenotypeAge", cols2Keep),
+    mergeSigResults("GenotypeAge", rrbsSigResults$Interaction, "RRBS", delta$Interaction$rrbs, "p.val.Interaction", "FDR_adj_interaction", cols2Keep)
+  ) %>% arrange(P.value_GenotypeAge)
+  
+  # Process Pathology data 
+  message("Merge ECX Pathology")
+  sig_ECX_Pathology <- rbind(
+    mergeSigResults("Pathology", arraySigResults$ECX$Pathology, "Array", delta$Pathology$array, "PrZ.Pathology", "FDR_adj_Pathology", cols2Keep),
+    mergeSigResults("Pathology", rrbsSigResults$Pathology, "RRBS", delta$Pathology$rrbs, "p.val.Pathology", "FDR_adj_pathology", cols2Keep)
+  ) %>% arrange(P.value_Pathology)
+  
+  
+  # overlap of probes that are genotype only vs interaction
+  probe_acrossModels <- list(
+    GenotypeInteraction = intersect(sig_ECX_GenotypeAge$Position, sig_ECX_Genotype$Position),
+    GenotypeOnly = setdiff(sig_ECX_Genotype$Position, sig_ECX_GenotypeAge$Position),
+    InteractionOnly = setdiff(sig_ECX_GenotypeAge$Position, sig_ECX_Genotype$Position),
+    GenotypePathology = intersect(sig_ECX_Genotype$Position, sig_ECX_Pathology$Position)
+  )
+  
+  sig_ECX <- list(
+    Genotype = sig_ECX_Genotype %>% mutate(InteractionEffect = ifelse(Position %in% probe_acrossModels$GenotypeInteraction,TRUE,FALSE)),
+    GenotypeAge = sig_ECX_GenotypeAge %>% mutate(GenotypeEffect = ifelse(Position %in% probe_acrossModels$GenotypeInteraction,TRUE,FALSE)),
+    Pathology = sig_ECX_Pathology %>% mutate(GenotypeEffect = ifelse(Position %in% probe_acrossModels$GenotypePathology,TRUE,FALSE))
+  )
+  
+  
+  # merge beta dataset
+  common_samples <- intersect(colnames(rrbsBeta), colnames(arrayBeta))
+  sig_beta <- list(
+    Genotype = rbind(arrayBeta %>% filter(row.names(.) %in% sig_ECX$Genotype$Position) %>% dplyr::select(all_of(common_samples)),
+                     rrbsBeta %>% filter(row.names(.) %in% sig_ECX$Genotype$Position) %>% dplyr::select(all_of(common_samples))),
+    Pathology = rbind(arrayBeta %>% filter(row.names(.) %in% sig_ECX$Pathology$Position) %>% dplyr::select(all_of(common_samples)),
+                      rrbsBeta %>% filter(row.names(.) %in% sig_ECX$Pathology$Position) %>% dplyr::select(all_of(common_samples)))
+  )
+  
+  output <- list(delta, sig_ECX_Genotype, sig_ECX_GenotypeAge, sig_ECX_Pathology, sig_ECX, sig_beta)
+  names(output) <- c("delta","sig_ECX_Genotype", "sig_ECX_GenotypeAge", "sig_ECX_Pathology", "sig_ECX_Full", "sig_betaECX_Full")
+  return(output)
+}
+
+
+plot_DMP <- function(betaMatrix, phenotypeFile, position, interaction = FALSE, pathology = FALSE){
+  
+  color_Tg4510_TG <- "#00AEC9"
+  
+  dat <- betaMatrix %>% filter(row.names(betaMatrix) %in% position)
+  dat <- dat %>% tibble::rownames_to_column(., var = "position") %>% reshape2::melt(variable.name = "sample",value.name = "methylation", id = "position")
+  dat <- merge(dat, phenotypeFile, by.y = 0, by.x = "sample")
+
+  
+  if(isTRUE(interaction)){
+    p <- ggplot(dat, aes(x = Age_months, y = methylation)) + 
+      geom_point(aes(colour = Genotype)) + 
+      scale_fill_manual(values = c(alpha("black",0.2), color_Tg4510_TG),guide="none") +
+      stat_summary(data=dat, aes(x=Age_months, y=methylation, group=Genotype, colour=Genotype), fun ="mean", geom="line", linetype = "dotted") +
+      labs(y = "Methylation", x = "Age (months)") + theme_classic() + 
+      scale_colour_manual(values = c("black", color_Tg4510_TG),guide="none") +
+      theme(panel.border = element_rect(fill = NA, color = "grey", linetype = "dotted"),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            strip.background = element_blank()) 
+  
+  }else if(isTRUE(pathology)){
+     p <- ggplot(dat, aes(x = ECX, y = methylation)) + 
+      geom_point(aes(colour = Genotype)) + 
+      scale_colour_manual(values = c(alpha("black",0.2), color_Tg4510_TG),guide="none") +
+      labs(x = "Pathology", y = "Methylation") +
+      geom_smooth(method=lm, formula = y~poly(x,3),colour="black",fill = alpha("gray",0.8))
+
+  }else{
+    p <- ggplot(dat, aes(x = Genotype, y = methylation, fill = Genotype)) + geom_boxplot(outlier.shape = NA) +
+      scale_fill_manual(values = c(alpha("black",0.2), color_Tg4510_TG),guide="none") +
+      scale_colour_manual(values = c("black", color_Tg4510_TG),guide="none") +
+      labs(x = "Genotype", y = "Methylation") 
+  }
+  
+  if(length(position > 1)){
+    p <- p + facet_grid(~position)
+  }
+  
+  p <- p +
+    theme_classic() + 
+    theme(panel.border = element_rect(fill = NA, color = "grey", linetype = "dotted"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.background = element_blank()) 
+
+  
+  return(p)
+}
+
+
+mergeSigResults <- function(test, sigResults, platform_name, logFC_data, value_col, fdr_col, cols_to_keep) {
+  
+  if(is.null(sigResults)){
+    dat <- data.frame(matrix(ncol = 5 + length(cols_to_keep), nrow = 0))
+    colnames(dat) <- c("Platform", "Position", "cpg", paste0("P.value_",test), paste0("FDR_adj_",test), cols_to_keep)
+    
+  }else{
+    if(platform_name == "Array"){
+      sigResults <- sigResults %>% mutate(platform = platform_name)
+    }else{
+      sigResults <- sigResults %>% mutate(platform = platform_name, cpg = NA)
+    }
+    dat <- sigResults %>%
+      mutate(position = Position) %>%
+      merge(., logFC_data, by = "position", all = TRUE) %>%
+      dplyr::select(platform, position, cpg, !!value_col, !!fdr_col, all_of(cols_to_keep)) %>%
+      setNames(c("Platform", "Position", "cpg", paste0("P.value_",test), paste0("FDR_adj_",test), cols_to_keep))
+  }
+  
+  
+  return(dat)
+}
+
+
+plot_platform_commonProbes <- function(rrbsBeta, arrayBeta, SigResults, phenotypeInput, commonProbes){
+  
+  p <- rbind(
+    extract_delta(betaMatrix=rrbsBeta,
+                  phenotypeFile=phenotypeInput,
+                  position=intersect(SigResults$Position, commonProbes)) %>% 
+      mutate(platform = "RRBS"),
+    extract_delta(betaMatrix=arrayBeta,
+                  phenotypeFile=phenotypeInput,
+                  position=intersect(SigResults$Position, commonProbes)) %>% 
+      mutate(platform = "Array")
+  ) %>% dplyr::select(-meanTG, -meanWT, -directionTG) %>% tidyr::spread(., platform, delta) %>% 
+    ggplot(., aes(x = Array, y = RRBS)) + geom_point() +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    mytheme + labs(x = "Array - delta", y = "RRBS - delta")
+  
+  return(p)
+}
