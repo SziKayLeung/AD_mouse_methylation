77f372cc0b7d039115930bd0437f75ce389024d2
sl693 <sl693@exeter.ac.uk>
2024-08-22 16:59:00 +0100

    remove human files

--- 7_HumanGeneListComparisons/function_uniqueAnno.r
+++
@@ -1,9 +0,0 @@
-
-#define unique anno function
-uniqueAnno<-function(row){
-  if(row != ""){
-    return(paste(unique(unlist(strsplit(row, "\\;"))), collapse = ";"))
-  } else {
-    return(row)
-  }
-}
